0973cf3e587ff4e1e5e885feea2d67c511caf955
Vincenzo Pellegrini <vincenzo.pellegrini@sonarsource.com>
2024-07-03 10:22:17 +0200

    Fix scanner suffix

--- build-analyze.ps1
+++ build-analyze.ps1
@@ -41,7 +41,7 @@ git fetch --unshallow
 
 $env:SONAR_SCANNER_VERSION = "6.1.0.4477"
 $env:SONAR_DIRECTORY = [System.IO.Path]::Combine($(get-location).Path,".sonar")
-$env:SONAR_SCANNER_HOME = "$env:SONAR_DIRECTORY/sonar-scanner-$env:SONAR_SCANNER_VERSION-windows"
+$env:SONAR_SCANNER_HOME = "$env:SONAR_DIRECTORY/sonar-scanner-$env:SONAR_SCANNER_VERSION-windows-x64"
 rm $env:SONAR_SCANNER_HOME -Force -Recurse -ErrorAction SilentlyContinue
 New-Item -path $env:SONAR_SCANNER_HOME -type directory
 (New-Object System.Net.WebClient).DownloadFile("https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$env:SONAR_SCANNER_VERSION-windows-x64.zip", "$env:SONAR_DIRECTORY/sonar-scanner.zip")
